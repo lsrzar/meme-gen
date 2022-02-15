86cff6533b96c6fcb7d8fb6af579b1d2b325f45d
lesrz <suarezarnaldi.leo@gmail.com>
2022-02-15 20:09:15 -0300

    Remove unnecessary element nesting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,15 @@ import './styles/Navbar.scss';
 
 const Navbar: React.FC = () => {
   return (
-    <div className='Navbar'>
-      <header className='Navbar-header'>
-        <div className='Navbar-brand'>
-          <a className='Navbar-logo-link' href='/'>
-            <img src={logo} className='Navbar-logo' alt='logo' />
-            <p className='Navbar-title'>Meme Generator</p>
-          </a>
-        </div>
-        <p className='Navbar-desc'>Make memes</p>
-      </header>
-    </div>
+    <header className='Navbar-header'>
+      <div className='Navbar-brand'>
+        <a className='Navbar-logo-link' href='/'>
+          <img src={logo} className='Navbar-logo' alt='logo' />
+          <p className='Navbar-title'>Meme Generator</p>
+        </a>
+      </div>
+      <p className='Navbar-desc'>Make memes</p>
+    </header>
   );
 };
 
