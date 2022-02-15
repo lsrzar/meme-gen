2c553b46b2245b86da41d92762627d0706102579
lesrz <suarezarnaldi.leo@gmail.com>
2022-02-15 20:08:43 -0300

    Remove leftover comment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import './styles/Hero.scss';
 
-// const YellowTextField = styled(TextField)({
-//   margin: 'auto',
-//   '& label.Mui-focused': {
-//     color: 'rgb(255, 208, 21)',
-//   },
-//   '& .MuiInput-underline:after': {
-//     borderBottomColor: 'rgb(255, 208, 21)',
-//   },
-//   '& .MuiOutlinedInput-root': {
-//     width: '230px',
-//     height: '55px',
-//     padding: '10px',
-//     '&:hover fieldset': {
-//       borderColor: 'rgb(255, 208, 21)',
-//     },
-//     '&.Mui-focused fieldset': {
-//       borderColor: 'rgb(255, 208, 21)',
-//     },
-//   },
-// });
-
 const Hero: React.FC = () => {
   return (
     <main className='Hero'>
